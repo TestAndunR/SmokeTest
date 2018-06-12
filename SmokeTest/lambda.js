20453f0813074b40dbf29419a0453e950921df33
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-06-12 09:53:34 +0530

    remove ddb and deploy

--- SmokeTest/lambda.js
+++ SmokeTest/lambda.js
@@ -3,16 +3,8 @@ const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 
 	console.log("Success");
-	ddb.put({
-		TableName: 'Smoke',
-		Item: {}
-	}, function (err, data) {
-		if (err) {
-			//handle error
-		} else {
-			//your logic goes here
-		}
-	});
+
+	
 
 	callback(null, 'Successfully executed');
 }
