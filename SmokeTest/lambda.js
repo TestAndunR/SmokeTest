d51b314eedd6f5e693b9a14a10fe49685399f85d
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-07-03 11:22:38 +0530

    aaaa

--- SmokeTest/lambda.js
+++ SmokeTest/lambda.js
@@ -1,31 +1,18 @@
 let AWS = require('aws-sdk');
-let SL_AWS = require('slappforge-sdk-aws');
-let connectionManager = require('./ConnectionManager');
-const rds = new SL_AWS.RDS(connectionManager);
-const ddb = new AWS.DynamoDB.DocumentClient();
+const s3 = new AWS.S3();
 exports.handler = function (event, context, callback) {
-
-	console.log("Success");
-
-	// You can pass the existing connection to this function.
-	// A new connection will be created if it's not present as the third param 
-	// You must always end/destroy the DB connection after it's used
-	rds.query({
-		instanceIdentifier: 'smokeDB',
-		query: 'aaa',
-		inserts: [aaa]
-	}, function (error, results, connection) {
-		if (error) {
-			console.log("Error occurred");
-			throw error;
-		} else {
-			console.log("Success")
-			console.log(results);
-		}
-
-		connection.end();
-	});
-
+	s3.listObjects({
+		'Bucket': 'testrt2.images',
+		'MaxKeys': 10,
+		'Prefix': ''
+	}).promise()
+		.then(data => {
+			console.log(data);           // successful response
+			
+		})
+		.catch(err => {
+			console.log(err, err.stack); // an error occurred
+		});
 
 
 	callback(null, 'Successfully executed');
